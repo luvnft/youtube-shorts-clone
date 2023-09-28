26f880a21d6df9c88150860e733df7c14231d59a
lynii <d0228582@gmail.com>
2023-09-28 22:58:52 +0800

    feat: add swipedown and swipeup

--- src/CarouselProvider.tsx
+++ src/CarouselProvider.tsx
@@ -1,12 +1,74 @@
-import { PropsWithChildren, createContext } from "react";
+import {
+  PropsWithChildren,
+  createContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import styles from "./carouselProvider.module.css";
+import Hammer from "hammerjs";
 
 export const Carousel = createContext<string>("");
 
 const CarouselProvider = ({ children }: PropsWithChildren) => {
+  const ref = useRef(null);
+  const [debug, setDebug] = useState("");
+  const [currentItemIndex, setCurrentItemIndex] = useState(0);
+
+  const handleSwipeUp = () => {
+    // current index + 1
+    setCurrentItemIndex((prev) => {
+      // MODIFY TO ITEMS LENGTH
+      if (prev + 1 >= 2) {
+        return 2;
+      } else {
+        return prev + 1;
+      }
+    });
+    setDebug(" up  gesture detected.");
+  };
+
+  const handleSwipeDown = () => {
+    // current index - 1
+    setCurrentItemIndex((prev) => {
+      // MODIFY TO ITEMS LENGTH
+      if (prev - 1 < 0) {
+        return 0;
+      } else {
+        return prev - 1;
+      }
+    });
+
+    setDebug(" down  gesture detected. ");
+  };
+
+  useEffect(() => {
+    const mc = new Hammer(ref.current ?? document.body);
+    mc.get("swipe").set({ direction: Hammer.DIRECTION_ALL });
+    mc.on("swipeup", handleSwipeUp);
+    mc.on("swipedown", handleSwipeDown);
+
+    return () => {
+      mc.destroy();
+    };
+  }, []);
+
   return (
     <Carousel.Provider value={styles.itemContainer}>
-      <div className={styles.container}>
+      <div
+        style={{
+          position: "fixed",
+          background: "#fff",
+          color: "#000",
+          zIndex: 600,
+          right: 0,
+          top: 0,
+        }}
+      >
+        {debug}
+        currentIndex: {currentItemIndex}
+      </div>
+      <div className={styles.container} ref={ref}>
         {children}
       </div>
     </Carousel.Provider>
