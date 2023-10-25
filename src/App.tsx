cb9bec97f62cd1511696f87686fa101ead6bde47
lynii <d0228582@gmail.com>
2023-10-25 19:31:10 +0800

    feat: add desktop ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,49 @@
-import "./App.css";
+import styles from "./app.module.css";
 import Tab from "./Tab";
 import TabProvider, { Id as TabId } from "./TabProvider";
 import UserConfigProvider from "./UserConfigProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import MainContent from "./MainContent";
+import { useMediaQuery } from "@uidotdev/usehooks";
 
 const queryClient = new QueryClient();
+const ASIDE_WIDTH = 280;
 
 function App() {
+  const isDesktopOrLaptop = useMediaQuery(
+    "only screen and (min-width : 769px)"
+  );
+  const mainWidth = isDesktopOrLaptop ? `480px` : "100%";
+  const mainTranslateX = isDesktopOrLaptop ? `${ASIDE_WIDTH / 2}px` : "0px";
+
   return (
     <QueryClientProvider client={queryClient}>
       <UserConfigProvider>
         <TabProvider>
-          <header>
+          <header className={styles.header}>
             <img src="/vite.svg" alt="log" />
             <Tab tabId={TabId.FOLLOWING}>following</Tab>
             <Tab tabId={TabId.FOR_YOU}>for you</Tab>
           </header>
-          <MainContent />
+          <MainContent
+            className={styles.main}
+            style={{
+              width: mainWidth,
+              transform: `translateX(${mainTranslateX})`,
+            }}
+          />
+          {isDesktopOrLaptop && (
+            <aside
+              className={styles.aside}
+              style={{ width: `${ASIDE_WIDTH}px` }}
+            >
+              <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+              </ul>
+            </aside>
+          )}
           <nav></nav>
         </TabProvider>
         <footer></footer>
--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import CarouselItem from "./CarouselItem";
 import CarouselProvider from "./CarouselProvider";
 import ShortVideoInformation from "./ShortVideoInformation";
@@ -8,8 +7,15 @@ import ShortVideoControl from "./ShortVideoControl";
 import { TabContainer } from "./Tab";
 import { useQuery } from "@tanstack/react-query";
 import { fetchFollowingList, fetchForYouList, mapToCarouselType } from "./api";
+import { CSSProperties, PropsWithChildren } from "react";
 
-const MainContent = () => {
+const MainContent = ({
+  className,
+  style,
+}: PropsWithChildren & {
+  className: string;
+  style: { [key: string]: string };
+}) => {
   const { data: followingList = [] } = useQuery({
     queryKey: ["following"],
     queryFn: fetchFollowingList,
@@ -20,45 +26,47 @@ const MainContent = () => {
   });
 
   return (
-    <main>
-      <TabContainer>
-        <CarouselProvider items={followingList.map(mapToCarouselType)}>
-          {followingList.map((item, i) => {
-            const { play_url, cover, title } = item;
-            return (
-              <CarouselItem key={i}>
-                <ShortVideoProvider>
-                  <ShortVideo
-                    index={i}
-                    video={{ src: play_url }}
-                    image={{ alt: title, src: cover }}
-                  />
-                  <ShortVideoInformation />
-                  <ShortVideoControl />
-                </ShortVideoProvider>
-              </CarouselItem>
-            );
-          })}
-        </CarouselProvider>
-        <CarouselProvider items={forYouList.map(mapToCarouselType)}>
-          {forYouList.map((item, i) => {
-            const { play_url, cover, title } = item;
-            return (
-              <CarouselItem key={i}>
-                <ShortVideoProvider>
-                  <ShortVideo
-                    index={i}
-                    video={{ src: play_url }}
-                    image={{ alt: title, src: cover }}
-                  />
-                  <ShortVideoInformation />
-                  <ShortVideoControl />
-                </ShortVideoProvider>
-              </CarouselItem>
-            );
-          })}
-        </CarouselProvider>
-      </TabContainer>
+    <main className={className} style={style as CSSProperties}>
+      <div>
+        <TabContainer>
+          <CarouselProvider items={followingList.map(mapToCarouselType)}>
+            {followingList.map((item, i) => {
+              const { play_url, cover, title } = item;
+              return (
+                <CarouselItem key={i}>
+                  <ShortVideoProvider>
+                    <ShortVideo
+                      index={i}
+                      video={{ src: play_url }}
+                      image={{ alt: title, src: cover }}
+                    />
+                    <ShortVideoInformation />
+                    <ShortVideoControl />
+                  </ShortVideoProvider>
+                </CarouselItem>
+              );
+            })}
+          </CarouselProvider>
+          <CarouselProvider items={forYouList.map(mapToCarouselType)}>
+            {forYouList.map((item, i) => {
+              const { play_url, cover, title } = item;
+              return (
+                <CarouselItem key={i}>
+                  <ShortVideoProvider>
+                    <ShortVideo
+                      index={i}
+                      video={{ src: play_url }}
+                      image={{ alt: title, src: cover }}
+                    />
+                    <ShortVideoInformation />
+                    <ShortVideoControl />
+                  </ShortVideoProvider>
+                </CarouselItem>
+              );
+            })}
+          </CarouselProvider>
+        </TabContainer>
+      </div>
     </main>
   );
 };
